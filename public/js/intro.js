6cfa56a2de3a897208d1ef29e26433ce8dadec0c
Victoria Guedes <vicguedes@yahoo.com.br>
2020-06-07 12:12:36 -0300

    Adicionando projeto Ecoleta ao GitHub

---
+++ public/js/intro.js
@@ -0,0 +1,70 @@
+// document.write("Hello, world")
+
+// var myvar // undefined
+// myvar = "Hello, world!"
+
+// document.write(myvar)
+
+// const n1 = 1
+// const n2 = 12 
+
+// document.write(n1 + n2)
+
+// * *  Objetos possuem propriedades e funcionalidades * * 
+
+// const pessoa = {
+//   altura: "1,66m",
+//   idade: 25,
+//   solteiro: false,
+  
+//   correr () {
+//     document.write("Run Forrest")
+//   }
+// }
+
+// pessoa.correr()
+
+
+// * * * * Array - Vetores * * * * * Coleção de dados
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "Victoria"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// * * * * Funções - Funcionalidades * * * * 
+
+// função registrada
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // executar
+// let i;
+
+// for (i=0 ; i<3 ;i++) {
+//   sayMyName("Douglas")
+// }
+
+// * * * * condicionais * * * * 
+
+// const notaFinal = 7
+
+// if (true) {
+//   // caminho 1 
+// } else {
+//   // caminho 2
+// } 
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado") 
+// } else {
+//   document.write("Aprovado") 
+// } 
+
+
+// * * * * loop * * * * 
+
+// for (i = 0; i < 10 ; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
